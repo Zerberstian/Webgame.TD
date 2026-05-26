2fb8ebdd8aadc611f955d10dbe8e133439b0db93
Zerberstian <zerberstian681@gmail.com>
2026-05-26 13:09:42 +0200

    Refactor WebG.js to remove duplicate code

--- js/WebG.js
+++ js/WebG.js
@@ -1,4 +1,30 @@
-alert("JS LOADED");
+// check for laoding the script (if needed)
+//alert("JS LOADED");
+
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", resizeCanvas);
+
+resizeCanvas();
+
+function showScreen(id) {
+
+    document.querySelectorAll(".screen").forEach(screen => {
+        screen.classList.add("hidden");
+    });
+
+    document.getElementById(id).classList.remove("hidden");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    showScreen("main-menu");
+});alert("JS LOADED");
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
