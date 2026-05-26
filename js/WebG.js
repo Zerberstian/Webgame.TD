1fda7db6f71e07a695d64366cc31b676825a541b
Zerberstian <zerberstian681@gmail.com>
2026-05-26 13:20:56 +0200

    Remove duplicate code and clean up comments

--- js/WebG.js
+++ js/WebG.js
@@ -1,5 +1,5 @@
-// check for laoding the script (if needed)
-//alert("JS LOADED");
+// check for loading the script
+// alert("JS LOADED");
 
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
@@ -14,31 +14,6 @@ window.addEventListener("resize", resizeCanvas);
 resizeCanvas();
 
 function showScreen(id) {
-
-    document.querySelectorAll(".screen").forEach(screen => {
-        screen.classList.add("hidden");
-    });
-
-    document.getElementById(id).classList.remove("hidden");
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    showScreen("main-menu");
-});alert("JS LOADED");
-const canvas = document.getElementById("game-canvas");
-const ctx = canvas.getContext("2d");
-
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-
-window.addEventListener("resize", resizeCanvas);
-
-resizeCanvas();
-
-function showScreen(id) {
-
     document.querySelectorAll(".screen").forEach(screen => {
         screen.classList.add("hidden");
     });
