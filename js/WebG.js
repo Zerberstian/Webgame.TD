5980c973fb2b86cef09410e8e9e22e7720e300a6
Zerberstian <zerberstian681@gmail.com>
2026-05-26 12:27:37 +0200

    Add files via upload

---
+++ js/WebG.js
@@ -0,0 +1,25 @@
+alert("JS LOADED");
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", resizeCanvas);
+
+resizeCanvas();
+
+function showScreen(id) {
+
+    document.querySelectorAll(".screen").forEach(screen => {
+        screen.classList.add("hidden");
+    });
+
+    document.getElementById(id).classList.remove("hidden");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    showScreen("main-menu");
+});
