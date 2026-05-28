c5bcf86e2d55dd61f5b75ba6f5f274d123fe8e57
Zerberstian <zerberstian681@gmail.com>
2026-05-28 10:44:22 +0200

    new file: for tower class, and removed old drawings from canvas

--- js/main.js
+++ js/main.js
@@ -9,26 +9,20 @@ function initGame() {
     if (gameInitialized) return; // Nur einmal ausführen
 
     // Beispiel-Spielobjekte: Türme, Gegner, Mauern
-    const toper1 = new Rect(100, 120, 80, 80, 'deepskyblue');
-    const toper2 = new Rect(200, 120, 80, 80, 'deepskyblue');
-    const enemy = new Circle(320, 180, 40, 'orangered');
-    const wall = new Rect(200, 300, 300, 30, 'darkgreen');
-    
+    //const toper1 = new Rect(100, 120, 80, 80, 'deepskyblue');
+
     // Beispiel: freie Form
-    const shape1 = new FreeformShape(400, 400, 'purple');
-    shape1.addPoint(0, 0);
-    shape1.addPoint(50, -30);
-    shape1.addPoint(100, 0);
-    shape1.addPoint(80, 50);
-    shape1.addPoint(20, 50);
+    //const shape1 = new FreeformShape(400, 400, 'purple');
+    //shape1.addPoint(0, 0);
+    //shape1.addPoint(50, -30);
+    //shape1.addPoint(100, 0);
+    //shape1.addPoint(80, 50);
+    //shape1.addPoint(20, 50);
 
     // Registriere Objekte bei Canvas zum Zeichnen
-    gameCanvas.add(toper1);
-    gameCanvas.add(toper2);
-    gameCanvas.add(enemy);
-    gameCanvas.add(wall);
-    gameCanvas.add(shape1);
-
+    //gameCanvas.add(toper1);
+    //gameCanvas.add(shape1);
+    
     gameInitialized = true;
 }
 
