36be99f0c226572713999806bf6e56cf9d93a3a4
Zerberstian <zerberstian681@gmail.com>
2026-05-28 13:03:06 +0200

    edit: better constructoer for tower class

--- js/entities/tower.js
+++ js/entities/tower.js
@@ -1,7 +1,9 @@
 class Tower {
-    constructor(type, range) {
+    constructor(type, range, position) {
         this.type = type; // Turmtyp (z.B. "basic", "sniper", "cannon")
         this.range = range;   // Angriffsreichweite  
-
+        this.damage = this.getDamageByType(type); // Schaden basierend auf dem Turmtyp
+        this.fireRate = this.getFireRateByType(type); // Feuerrate basierend auf dem Turmtyp
+        this.lastShotTime = 0; // Zeit des letzten Schusses
     }
 }
