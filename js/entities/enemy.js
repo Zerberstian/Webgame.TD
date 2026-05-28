9649f5201284cda961df94308bbd7f85d59fb3bc
Zerberstian <zerberstian681@gmail.com>
2026-05-28 15:24:54 +0200

    added: class constructor for enemies

--- js/entities/enemy.js
+++ js/entities/enemy.js
@@ -1,1 +1,11 @@
 // added later
+console.log("enemy.js loaded");
+
+class Enemy {
+    constructor(type, health, speed, position) {
+        this.type = type;   // Gegnertyp (z.B. "basic", "fast", "tank")
+        this.health = health; // Lebenspunkte
+        this.speed = speed;   // Bewegungsgeschwindigkeit
+        this.position = position; // Aktuelle Position (z.B. {x: 0, y: 0})
+    }
+}
