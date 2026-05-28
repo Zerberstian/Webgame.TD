1e8e3c09fa864d0708da92a4ebab5a69735cac2f
Zerberstian <zerberstian681@gmail.com>
2026-05-28 10:59:27 +0200

    removed: ai inserted code

---
+++ js/entities/tower.js
@@ -0,0 +1,7 @@
+class Tower {
+    constructor(type, range) {
+        this.type = type; // Turmtyp (z.B. "basic", "sniper", "cannon")
+        this.range = range;   // Angriffsreichweite  
+
+    }
+}
